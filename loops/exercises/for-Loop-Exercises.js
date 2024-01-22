eced7854ed017d0bcfb0dd193c3ea7f8793c0e31
kvnsh <kaveh.noshadi@gmail.com>
2024-01-22 17:06:53 -0600

    updated for loop exercises

--- loops/exercises/for-Loop-Exercises.js
+++ loops/exercises/for-Loop-Exercises.js
@@ -4,21 +4,21 @@
     c. Print the EVEN numbers 12 to -14 in descending order, one number per line.
     d. Challenge - Print the numbers 50 - 20 in descending order, but only if the numbers are multiples of 3. (Your code should work even if you replace 50 or 20 with other numbers). */
 // a
-//     for(i = 0; i < 21; i++){
-//   console.log(i);
-// }
+    for(i = 0; i < 21; i++){
+  console.log(i);
+}
 
 // b 
-// for(i = 3; i <= 29; i++){
-//   if (i % 2 !== 0){
-//   console.log(i);
-//   }
-// }
+for(i = 3; i <= 29; i++){
+  if (i % 2 !== 0){
+  console.log(i);
+  }
+}
 
 // c 
-// for (let i = 12; i >= -14; i-=2) {
-//   console.log(i);
-// }
+for (let i = 12; i >= -14; i-=2) {
+  console.log(i);
+}
 
 // d 
 for(i = 50; i > 19; i-=1){
@@ -28,18 +28,41 @@ for(i = 50; i > 19; i-=1){
 }
 
 
-/*Exercise #2: 
-Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
+// Exercise #2: 
+// Initialize two variables to hold the string “LaunchCode” and the array [1, 5, ‘LC101’, ‘blue’, 42].
+
+let myString = "Launchcode";
+let arrayName = [1, 5, "LC101", "blue", 42];
+
+// Construct ``for`` loops to accomplish the following tasks:
+//   a. Print each element of the array to a new line.
+
+for( let i = 0; i < arrayName.length; i++){
+  console.log(arrayName[i]);
+}
+
+//   b. Print each character of the string - in reverse order - to a new line.
+
+for(let i = myString.length - 1; i>=0 ; i--){
+  console.log(myString[i]);
+}
 
 
-Construct ``for`` loops to accomplish the following tasks:
-  a. Print each element of the array to a new line.
-  b. Print each character of the string - in reverse order - to a new line. */ 
+// Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
+//   a. One array contains the even numbers, and the other holds the odds.
 
+let numbersArray = [2, 3, 13, 18, -5, 38, -10, 11, 0, 104];
+let evens = [];
+let odds = [];
 
+for (let i = 0; i < numbersArray.length; i++) {
+   if (numbersArray[i] % 2 === 0) {
+      evens.push(numbersArray[i]);
+   } else {
+      odds.push(numbersArray[i]);
+   }
+}
 
-
-
-/*Exercise #3:Construct a for loop that sorts the array [2, 3, 13, 18, -5, 38, -10, 11, 0, 104] into two new arrays:
-  a. One array contains the even numbers, and the other holds the odds.
-  b. Print the arrays to confirm the results. */
+  // b. Print the arrays to confirm the results. 
+  console.log(evens);
+console.log(odds);
